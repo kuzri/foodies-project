7784708bab7e1e5f82727807f23bb23b57987372
hs lee <dlgudtn032@naver.com>
2024-08-11 01:47:43 +0900

    update loading page and sqllite database & get data

---
+++ app/loading.js
@@ -0,0 +1,11 @@
+
+import classes from './loading.module.css'
+
+export default function LoadingPage() {
+    return (
+        <p className={classes.loading}>
+            Fetching meals...   
+        </p>
+    );
+}
+
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,12 +1,15 @@
 import Link from 'next/link';
 import classes from './page.module.css'
 import MealsGrid from '../components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
 
-export default function MealsPage(){
-    return <>
+export default async function MealsPage(){
+    const meals = await getMeals();
+    return (
+        <>
         <header className={classes.header}>
             <h1>
-                Delicious meals, created(' ')
+                Delicious meals, created{' '}
                 <span className={classes.hightlight}>by you</span>
             </h1>
             <p>Choose  your fabvorite recipe and cook it</p>
@@ -17,7 +20,8 @@ export default function MealsPage(){
                 </p>
         </header>
         <main className={classes.main}>
-            <MealsGrid meals={[]}/>
+            <MealsGrid meals={meals}/>
         </main>
     </>
+    )
 }
